cce62903d3da45afad82c49dda731a4f8cc2d698
afzalhaider1 <131112928+afzalhaider1@users.noreply.github.com>
2025-02-06 22:14:12 +0530

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -51,7 +51,7 @@ def AddEmp():
 
     try:
         
-        cursor.execute(insert_sql,(empid, first_name, last_name, pri_skill, location))
+        cursor.execute(insert_sql,(emp_id, first_name, last_name, pri_skill, location))
         db_conn.commit()
         emp_name = "" + first_name + " " + last_name
         # Uplaod image file in S3 #
